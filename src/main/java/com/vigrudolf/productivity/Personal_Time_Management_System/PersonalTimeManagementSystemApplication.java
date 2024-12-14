74050df5a311d157e764cff525a27d508d5ce3c2
vigRudolf <vrudi68@gmail.com>
2024-12-14 14:55:32 +0100

    Initial commit

---
+++ src/main/java/com/vigrudolf/productivity/Personal_Time_Management_System/PersonalTimeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vigrudolf.productivity.Personal_Time_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTimeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTimeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vigrudolf/productivity/Personal_Time_Management_System/PersonalTimeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigrudolf.productivity.Personal_Time_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalTimeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
