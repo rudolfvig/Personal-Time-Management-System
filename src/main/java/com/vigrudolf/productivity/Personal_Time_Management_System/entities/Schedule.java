e7c387c98c33a5678c6c3585a9e7e0a078926a5f
vigRudolf <vrudi68@gmail.com>
2024-12-14 16:11:52 +0100

    Schedule Entity

--- src/main/java/com/vigrudolf/productivity/Personal_Time_Management_System/entities/Schedule.java
+++ src/main/java/com/vigrudolf/productivity/Personal_Time_Management_System/entities/Schedule.java
@@ -1,4 +1,55 @@
 package com.vigrudolf.productivity.Personal_Time_Management_System.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Schedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "title is required")
+    @Column(unique = true)
+    private String title;
+
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(nullable = false)
+    private LocalDateTime endTime;
+
+    @Enumerated(EnumType.STRING)
+    private Priority priority;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public enum Priority{
+        HIGH, MEDIUM, LOW
+    }
+
+    public enum Status{
+        TODO, IN_PROGRESS, COMPLETED
+    }
+    
 }
