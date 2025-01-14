a8806440626dbe6d2ff8d6058571f56547a62f35
vigRudolf <vrudi68@gmail.com>
2025-01-14 11:09:18 +0100

    abstract Task entity

--- src/main/java/com/vigrudolf/productivity/Personal_Time_Management_System/entities/Task.java
+++ src/main/java/com/vigrudolf/productivity/Personal_Time_Management_System/entities/Task.java
@@ -1,23 +1,23 @@
 package com.vigrudolf.productivity.Personal_Time_Management_System.entities;
 
 import com.vigrudolf.productivity.Personal_Time_Management_System.enums.TaskPriority;
+import com.vigrudolf.productivity.Personal_Time_Management_System.enums.TaskStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 
-@Entity
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class Task {
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "task_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,30 +27,23 @@ public class Task {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @NotBlank(message = "title is required")
+    @NotBlank(message = "Title is required")
     @Column(unique = true)
     private String title;
 
+    @Size(max = 500)
     private String description;
 
     @Column(nullable = false)
     private LocalDateTime startTime;
 
-    @Column(nullable = false)
-    private LocalDateTime endTime;
-
     @Enumerated(EnumType.STRING)
     private TaskPriority priority;
 
     @Enumerated(EnumType.STRING)
-    private TaskPriority status;
+    private TaskStatus taskStatus;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
-
-
-
-
-
 }
